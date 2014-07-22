35c4911f48954eb1122ec5456e8d828e48e7839f
Alex Blount <alexkblount@gmail.com>
2014-07-22 14:33:53 +1000

    initial commit

---
+++ FunWithEffects/CircularEffectButton.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using MonoTouch.UIKit;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.CoreGraphics;
+
+namespace FunWithEffects
+{
+	public class CircularEffectButton
+	 : UIButton
+	{
+		private UIVisualEffectView effectView;
+
+		public CircularEffectButton (PointF location, float size, UIVisualEffect effect, UIColor backgroundColor, string title, UIColor titleColor)
+			: base(UIButtonType.Custom)
+		{
+
+			this.Frame = new RectangleF(location, new SizeF(size, size));
+			this.effectView = new UIVisualEffectView (effect);
+			this.effectView.Frame = new RectangleF(new PointF(0,0), this.Frame.Size);
+			this.effectView.UserInteractionEnabled = false;
+			this.Add (this.effectView);
+
+			this.BackgroundColor = backgroundColor.ColorWithAlpha(0.2f);
+			this.SetTitle (title, UIControlState.Normal);
+			var contrastingColor = this.ContrasingColor (backgroundColor);
+			this.SetTitleColor (contrastingColor, UIControlState.Normal);
+			this.TitleLabel.AdjustsFontSizeToFitWidth = true;
+			this.TitleLabel.TextAlignment = UITextAlignment.Center;
+			this.Layer.CornerRadius = size / 2;
+			this.Layer.BorderWidth = 1;
+			this.Layer.BorderColor = contrastingColor.CGColor;
+			this.ClipsToBounds = true;
+		}
+
+		private UIColor ContrasingColor(UIColor originalColor)
+		{
+			var originalCGColor = originalColor.CGColor;
+
+			var numberOfComponents = originalCGColor.NumberOfComponents;
+
+			if (numberOfComponents == 1) {
+				return new UIColor (originalCGColor);
+			}
+
+			var originalComponentColors = originalCGColor.Components;
+			var newComponentColors = new float[numberOfComponents];
+
+			var i = numberOfComponents - 1;
+
+			newComponentColors [i] = originalComponentColors [i];
+			i--;
+			while (i >= 0) {
+				newComponentColors[i] = 1 - originalComponentColors[i];
+				i--;
+			}
+
+			var newCGColor = new CGColor (originalCGColor.ColorSpace, newComponentColors);
+			var newUIColor = new UIColor (newCGColor);
+
+			return newUIColor;
+		}
+
+	}
+}
+
---
+++ FunWithEffects/FunWithEffectsViewController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.CoreAnimation;
+using MonoTouch.CoreGraphics;
+using MonoTouch.CoreLocation;
+using MonoTouch.MapKit;
+
+namespace FunWithEffects
+{
+	public partial class FunWithEffectsViewController : UIViewController
+	{
+		private UIVisualEffectView blurView;
+		private const int CIRCLE_RECT_SIZE = 250;
+
+		public FunWithEffectsViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning ();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			
+			this.blurView = new UIVisualEffectView (UIBlurEffect.FromStyle (UIBlurEffectStyle.Light));
+			this.blurView.Frame = this.View.Frame;
+			this.blurView.UserInteractionEnabled = false;
+			this.View.Add (this.blurView);
+
+			var maskLayer = new CAShapeLayer ();
+			var maskPath = new CGPath ();
+			maskPath.AddRect (this.blurView.Bounds);
+			maskPath.AddEllipseInRect (new RectangleF (((this.blurView.Bounds.Width - CIRCLE_RECT_SIZE) / 2), ((this.blurView.Bounds.Height - CIRCLE_RECT_SIZE) / 2), CIRCLE_RECT_SIZE, CIRCLE_RECT_SIZE));
+			maskLayer.Path = maskPath;
+			maskLayer.FillRule = CAShapeLayer.FillRuleEvenOdd;
+			this.blurView.Layer.Mask = maskLayer;
+
+			this.AddLocationButton (UIColor.Blue, "Sydney", 0, -33.8678500, 151.2073200);
+			this.AddLocationButton (UIColor.Purple, "Boston", 1, 42.3584300, -71.0597700);
+			this.AddLocationButton (UIColor.Green, "San Fran", 2, 37.7749300, -122.4194200);
+			this.AddLocationButton (UIColor.Red, "Seattle", 3, 47.6062100, -122.3320700);
+		}
+
+		private void AddLocationButton(UIColor buttonColor, string buttonText, int position, double latitude, double longitude)
+		{
+			var xPosition = 10 + (80 * position);
+
+			var button = new CircularEffectButton (
+				new PointF (xPosition, this.View.Frame.Height - 100), 
+				60, 
+				new UIVibrancyEffect(),
+				buttonColor, 
+				buttonText,
+				UIColor.White
+			);
+
+			button.TouchUpInside += (object sender, EventArgs e) => {
+				var geoLocation = new CLLocationCoordinate2D(latitude, longitude);
+				var span = new MKCoordinateSpan(0.3, 0.3);
+				var mapRegion = new MKCoordinateRegion(geoLocation, span);
+
+				this.MapView.SetRegion(mapRegion, true);
+
+			};
+			this.View.InsertSubviewAbove(button, this.blurView);
+		}
+
+		#endregion
+	}
+}
+
